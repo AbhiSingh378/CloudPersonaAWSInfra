af5445d9543c477070a165bf554a38ab9ecbab3a
Abhishek Singh <singh.abhishek3@northeastern.edu>
2024-10-17 23:54:06 -0400

    feat{updated infra}:updated infra

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -44,16 +44,15 @@ resource "aws_security_group" "app_sg" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = var.ami_id
-  instance_type = "t2.micro"
-  subnet_id     = var.public_subnet_ids[0]
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
-  
-  associate_public_ip_address = true  # Add this line
+  ami                         = var.ami_id
+  instance_type               = "t2.micro"
+  subnet_id                   = var.public_subnet_ids[0]
+  vpc_security_group_ids      = [aws_security_group.app_sg.id]
+  associate_public_ip_address = true
 
   root_block_device {
-    volume_size = 25
-    volume_type = "gp2"
+    volume_size           = 25
+    volume_type           = "gp2"
     delete_on_termination = true
   }
 
